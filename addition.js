f20e20e6f45953ed5009acf28c2d6673bfcfaa2b
Marianne Anderson <mganderson13@gmail.com>
2024-11-17 18:19:36 -0600

    addition file and logic outline

---
+++ addition.js
@@ -0,0 +1,35 @@
+//run when operation is pressed
+//create numberline of entire container length
+//place numA at beginning of numberline 
+//highlight num on number line
+
+
+
+//run when enter is pressed
+//fill in numberline
+//calculate lines- every 100, 10, and 1 for numB
+
+//numBArray = numB.split("")
+//if (numBArray.length === 3)
+// const hundreds = numBArray[0]
+// const tens = numBArray[1]
+// const ones = numBArray[2]
+//if (numBArray.length === 2)
+// const tens = numBArray[0]
+// const ones = numBArray[1]
+//if (numBArray.length === 1)
+// const ones = numBArray[0]
+
+//calculate numberline units = (hundreds(3)+tens(2)+ones)
+
+
+// if (hundreds) { create for each hundreds one div with length 3units}
+//numA = total display total+=100  at each jump
+ 
+// if (tens && tens >=1) { create for each tens one div with length 2units}
+//display total+=10 at each jump
+
+// if (ones >= 1) { create for each ones one div with length units}
+//display total+=1 at each jump
+
+//call arrow functions- display +100, +10, +1 with each jump 
